f53fbb06e580f0a659725ca54cb8723ee1e2899b
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-02-16 17:51:57 +0530

    infinite scroll on desktop site

--- script.js
+++ script.js
@@ -5,14 +5,40 @@ const loadterminate = () => {
   let images = document.getElementById('images')
   images.setAttribute("class", "invisible")
 }
-const clickhandler = (e) => {
+var Sno = 1
+let noOfImageToBeLoaded = 9
+
+const updater = (e) => {
+  Sno +=1
+  console.log("load new image",Sno,e)
   searchbar.value = ""
-  let urlupdated = url1 + searchitem + e
+  let loadingimage = document.getElementById('images')
+  loadingimage.setAttribute("class", "loading")
+  setTimeout(loadterminate, 1500)
+  let urlupdated = url1 + searchitem + e + "&" + Sno
   let image = document.createElement('img')
   image.setAttribute("src", urlupdated)
   let imagecontainer = document.getElementById('imagecontainer')
   imagecontainer.prepend(image)
-  let images = document.getElementById('images')
-  images.setAttribute("class", "loading")
-  setTimeout(loadterminate, 1500)
 }
+
+const clickhandler = (e) => {
+  for (let i = 0; i < noOfImageToBeLoaded; i++) {
+    updater(e)
+    
+  }
+}
+
+const scrollhandler = () => {
+  let scrolltop = document.documentElement.scrollTop
+  let totalheight = document.documentElement.scrollHeight
+  let windowheight = window.innerHeight
+  let position = windowheight + scrolltop
+  if (scrolltop > 400) {
+    if (totalheight < position) {
+      updater()
+    }
+  }
+}
+
+window.addEventListener('scroll', scrollhandler)
