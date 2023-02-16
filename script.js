efbf11a70523d4a70fff9b09c31412c56873f319
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-02-16 23:49:45 +0530

    click to enlarge

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let searchbar = document.getElementById("searchbar")
 let searchitem = searchbar.value
-let url1 = "https://source.unsplash.com/featured/320x240?"
+let url1 = "https://source.unsplash.com/featured/1600x900?"
+let imagecontainer = document.getElementById('imagecontainer')
+let cross = document.getElementById('cross')
+
 const loadterminate = () => {
   let images = document.getElementById('images')
   images.setAttribute("class", "invisible")
 }
 var Sno = 1
-let noOfImageToBeLoaded = 9
+let target
+let noOfImageToBeLoaded = 12
 
 const updater = (e) => {
-  Sno +=1
-  console.log("load new image",Sno,e)
+  Sno += 1
+  // console.log("load new image",Sno,e)
   searchbar.value = ""
   let loadingimage = document.getElementById('images')
   loadingimage.setAttribute("class", "loading")
@@ -18,15 +22,29 @@ const updater = (e) => {
   let urlupdated = url1 + searchitem + e + "&" + Sno
   let image = document.createElement('img')
   image.setAttribute("src", urlupdated)
-  let imagecontainer = document.getElementById('imagecontainer')
+  image.setAttribute("class", "image")
+  image.onclick = imageviewer
   imagecontainer.prepend(image)
 }
 
 const clickhandler = (e) => {
   for (let i = 0; i < noOfImageToBeLoaded; i++) {
     updater(e)
-    
+
   }
+}
+
+const imageviewer = (e) => {
+  console.log("image clicked",e.target)
+  target=e.target
+  e.target.setAttribute("class","enlarge")
+  cross.removeAttribute("class","invisible")
+}
+
+const imagedismiss =()=>{
+  cross.setAttribute("class","invisible")
+  target.removeAttribute("class","enlarge")
+  target.setAttribute("class","image")
 }
 
 const scrollhandler = () => {
@@ -42,3 +60,5 @@ const scrollhandler = () => {
 }
 
 window.addEventListener('scroll', scrollhandler)
+cross.addEventListener('click', imagedismiss)
+// imagecontainer.addEventListener('click', imageviewer)
