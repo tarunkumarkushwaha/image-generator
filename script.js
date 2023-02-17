967025597cdc7f5a2b9bcb99484084cdd60a538e
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-02-17 13:43:18 +0530

    Scroll bug fixed

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let searchitem = searchbar.value
 let url1 = "https://source.unsplash.com/featured/1600x900?"
 let imagecontainer = document.getElementById('imagecontainer')
 let cross = document.getElementById('cross')
+let image = document.getElementsByClassName('image')
 
 const loadterminate = () => {
   let images = document.getElementById('images')
@@ -10,11 +11,11 @@ const loadterminate = () => {
 }
 var Sno = 1
 let target
-let noOfImageToBeLoaded = 12
+let query 
+let noOfImageToBeLoaded = 9
 
 const updater = (e) => {
   Sno += 1
-  // console.log("load new image",Sno,e)
   searchbar.value = ""
   let loadingimage = document.getElementById('images')
   loadingimage.setAttribute("class", "loading")
@@ -28,37 +29,49 @@ const updater = (e) => {
 }
 
 const clickhandler = (e) => {
+  query=e
+  while (imagecontainer.hasChildNodes()) {
+    imagecontainer.removeChild(imagecontainer.firstChild);
+  }
   for (let i = 0; i < noOfImageToBeLoaded; i++) {
     updater(e)
-
   }
 }
 
 const imageviewer = (e) => {
-  console.log("image clicked",e.target)
-  target=e.target
-  e.target.setAttribute("class","enlarge")
-  cross.removeAttribute("class","invisible")
+  console.log("image clicked", e.target)
+  target = e.target
+  e.target.setAttribute("class", "enlarge")
+  cross.removeAttribute("class", "invisible")
 }
 
-const imagedismiss =()=>{
-  cross.setAttribute("class","invisible")
-  target.removeAttribute("class","enlarge")
-  target.setAttribute("class","image")
+const imagedismiss = () => {
+  cross.setAttribute("class", "invisible")
+  target.removeAttribute("class", "enlarge")
+  target.setAttribute("class", "image")
 }
 
 const scrollhandler = () => {
   let scrolltop = document.documentElement.scrollTop
   let totalheight = document.documentElement.scrollHeight
   let windowheight = window.innerHeight
-  let position = windowheight + scrolltop
+  let position = windowheight + scrolltop + 100
   if (scrolltop > 400) {
+    // console.log("totalheight- ",totalheight,"and position- ",position)
     if (totalheight < position) {
-      updater()
+      // console.log("load new images")
+      updater(query)
     }
+  }
+}
+
+const onEnterPress = (e) => {
+  if (e.keyCode == 13 && e.shiftKey == false) {
+    e.preventDefault();
+    clickhandler(searchbar.value)
   }
 }
 
 window.addEventListener('scroll', scrollhandler)
 cross.addEventListener('click', imagedismiss)
-// imagecontainer.addEventListener('click', imageviewer)
+searchbar.addEventListener('keydown',onEnterPress)
